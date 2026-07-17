7fea2ac555455e9af0b996fea2e49ddc87948438
Anjaly Sajeev <anjalysajeev1695@gmail.com>
2026-07-17 11:46:36 +0530

    add application class and Hilt setup

--- app/src/main/java/com/anjaly/tweetsy/MainActivity.kt
+++ app/src/main/java/com/anjaly/tweetsy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.anjaly.tweetsy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +12,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.anjaly.tweetsy.api.TweetsyAPI
 import com.anjaly.tweetsy.ui.theme.TweetsyTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var tweetsyAPI: TweetsyAPI
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        lifecycleScope.launch {
+            val response = tweetsyAPI.getCategories()
+            Log.d("TWEETSY APP", response.body()!!.distinct().toString())
+        }
         setContent {
             TweetsyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
---
+++ app/src/main/java/com/anjaly/tweetsy/Tweetsy.kt
@@ -0,0 +1,8 @@
+package com.anjaly.tweetsy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Tweetsy: Application() {
+}
