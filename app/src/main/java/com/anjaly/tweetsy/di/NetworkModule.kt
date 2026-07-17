fb3c265704ead245baac9e3105cb6f6f5a437ab6
Anjaly Sajeev <anjalysajeev1695@gmail.com>
2026-07-17 11:08:02 +0530

    build: integrate Hilt for dependency injection

---
+++ app/src/main/java/com/anjaly/tweetsy/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.anjaly.tweetsy.di
+
+import com.anjaly.tweetsy.api.TweetsyAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl("https://api.jsonbin.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTweetsyAPI(retrofit: Retrofit): TweetsyAPI {
+        return retrofit.create(TweetsyAPI::class.java)
+    }
+}
